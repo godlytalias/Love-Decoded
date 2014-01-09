21dca8032bc7060c1b321b4d2fc518d04054c4de
godlytalias <godlytalias@yahoo.co.in>
2014-01-09 23:56:10 +0530

    updated version

--- js/init.js
+++ js/init.js
@@ -1,13 +1,8 @@
-function song(){
-var clickSound = new Audio('silentnight.mp3');
-clickSound.play();
-setTimeout("song()",115000);
-}
-
 function toggleVideo(state,tag) {
     var div = document.getElementById(tag);
     var object = div.getElementsByTagName("object")[0].contentWindow;
-    div.style.display = state == 'hide' ? 'none' : '';
+div.getElementsByTagName("object")[0].style.display=state=='hide'?'none':'block';
+    div.style.display = state == 'hide' ? 'none' : 'block';
     func = state == 'hide' ? 'pauseVideo' : 'playVideo';
     object.postMessage('{"event":"command","func":"' + func + '","args":""}','*');
 }
